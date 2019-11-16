cb0e894c7f856755f2156267f09806af85fb25fd
William <fraptorwh98@WH.localdomain>
2019-11-16 16:00:42 +0700

    second commit

--- main.py
+++ main.py
@@ -1,94 +1,80 @@
-import tweepy
-import csv
-import json
-import datetime
+from tweepy import OAuthHandler
+from tweepy import API
+from tweepy import Cursor
+from datetime import datetime, date, time, timedelta
+from collections import Counter
+import sys
 
-# me-load Twitter API credentials yang sudah dituliskan di file twitter_credentials.json
+consumer_key="ShGU1J46KOaWtJm5IvgfY8hp9"
+consumer_secret="JyfvAE2udldVzCkxFEAFjvETJnWfclh2ZObQg9P1NVs58ndR1d"
+access_token="362485949-3GOV8E370bSqCd0qMmjzvEiwD0SuZwW38NwBiToO"
+access_token_secret="rTjYaVV6XIgNmCG0l1N41LdwBqeOapChzpksahxf0EaLA"
 
-with open('twitter_credentials.json') as cred_data:
-    info = json.load(cred_data)
-    consumer_key = info['CONSUMER_KEY']
-    consumer_secret = info['CONSUMER_SECRET']
-    access_key = info['ACCESS_KEY']
-    access_secret = info['ACCESS_SECRET']
-    
-#menampilkan seluruh tweets pada timeline user
-def get_all_tweets(screen_name):
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_key, access_secret)
-    api = tweepy.API(auth)
-    all_tweets = []
-    # maksimal 100 tweets
-    new_tweets = api.user_timeline(screen_name=screen_name, count=100)
-    # Melihat recent tweets
-    all_tweets.extend(new_tweets)
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+auth_api = API(auth)
 
-    # save id of 1 less than the oldest tweet
-    oldest_tweet = all_tweets[-1].id - 1
+account_list = []
+if (len(sys.argv) > 1):
+  account_list = sys.argv[1:]
+else:
+  print("Please provide a list of usernames at the command line.")
+  sys.exit(0)
 
-    while len(new_tweets) :
-        new_tweets = api.user_timeline(screen_name=screen_name,
-        count=100, max_id=oldest_tweet)
+if len(account_list) > 0:
+  for target in account_list:
+    print("Getting data for " + target)
+    item = auth_api.get_user(target)
+    print("name: " + item.name)
+    print("screen_name: " + item.screen_name)
+    print("description: " + item.description)
+    print("statuses_count: " + str(item.statuses_count))
+    print("friends_count: " + str(item.friends_count))
+    print("followers_count: " + str(item.followers_count))
 
-        # menyimpan tweet terbaru
-        all_tweets.extend(new_tweets)
+    tweets = item.statuses_count
+    account_created_date = item.created_at
+    delta = datetime.utcnow() - account_created_date
+    account_age_days = delta.days
+    print("Account age (in days): " + str(account_age_days))
+    if account_age_days > 0:
+      print("Average tweets per day: " + "%.2f"%(float(tweets)/float(account_age_days)))
 
-        oldest_tweet = all_tweets[-1].id - 1
-        print ('...%s tweets yang sudah didownload' % len(all_the_tweets))
+    hashtags = []
+    mentions = []
+    tweet_count = 0
+    end_date = datetime.utcnow() - timedelta(days=30)
+    for status in Cursor(auth_api.user_timeline, id=target).items():
+      tweet_count += 1
+      if hasattr(status, "entities"):
+        entities = status.entities
+        if "hashtags" in entities:
+          for ent in entities["hashtags"]:
+            if ent is not None:
+              if "text" in ent:
+                hashtag = ent["text"]
+                if hashtag is not None:
+                  hashtags.append(hashtag)
+        if "user_mentions" in entities:
+          for ent in entities["user_mentions"]:
+            if ent is not None:
+              if "screen_name" in ent:
+                name = ent["screen_name"]
+                if name is not None:
+                  mentions.append(name)
+      if status.created_at < end_date:
+        break
 
-        outtweets = [[tweet.id_str, tweet.created_at,
-        tweet.text.encode('utf-8')] for tweet in all_tweets]
+    print
+    print("Most mentioned Twitter users:")
+    for item, count in Counter(mentions).most_common(10):
+      print(item + "\t" + str(count))
 
-    with open(screen_name + '_tweets.csv', 'w', encoding='utf8') as f:
-        writer = csv.writer(f)
-        writer.writerow(['id', 'created_at', 'text'])
-        writer.writerows(outtweets)
+    print
+    print("Most used hashtags:")
+    for item, count in Counter(hashtags).most_common(10):
+      print(item + "\t" + str(count))
 
-    if __name__ == '__main__':
-        get_all_tweets(input("Menginput twitter handle dari user yang tweetnya hendak ditampilkan :"))
-        
-#menampilkan informasi akun user
-class TweetListener(StreamListener):
-    # A listener handles tweets are the received from the stream.
-    #This is a basic listener that just prints received tweets to standard output
-
-    def on_data(self, data):
-        print (data)
-        return True
-
-    def on_error(self, status):
-        print (status)
-api = tweepy.API(auth)
-twitterStream = Stream(auth,TweetListener())
-test = api.lookup_users(user_ids=['17006157','59145948','157009365'])
-for user in test:
-    print (user.screen_name)
-    print (user.name)
-    print (user.description)
-    print (user.followers_count)
-    print (user.statuses_count)
-    print (user.url)
-#menampilkan informasi berdasarkan mentions
-def save_mentions(self, tweet):
-    for mention in tweet['entities']['user_mentions']:
-        screen_name = USERS.find_one({'twitter': mention['screen_name']})
-        if AUTO_REPLY and not DEVELOPER_MODE:
-            # Send message to user if not in DEVELOPER_MODE
-            send_message = 'Sending message to @%s. %s' % (tweet['user']['screen_name'], datetime.datetime.now())
-            logging.info(send_message)
-            if VERBOSE:
-                print (send_message)
-                message_thread = TweetMaker(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET,
-                                            tweet['user']['screen_name'])
-                message_thread.start()
-            # If no user (mention) was found in our database let's save it
-            if not screen_name:
-                USERS.insert({
-                    'twitter': mention['screen_name'],
-                    'created': datetime.datetime.utcnow(),
-                    'mentioned_by': tweet['user']['screen_name']
-                })
-                added_message = 'Added @%s! %s' % (mention['screen_name'], datetime.datetime.now())
-                logging.info(added_message)
-                if VERBOSE:
-                    print (added_message)
+    print
+    print ("All done. Processed" + str(tweet_count) + " tweets.")
+    print
